6c6365195329c91a1b51b57e612040535a19ddb2
IsDaDev <115308963+IsDaDev@users.noreply.github.com>
2024-09-30 17:08:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,8 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 // starts the server on port 3000
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // ****************************************************
